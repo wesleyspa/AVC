5acf26f8ba5ab37ea41e9752633426b548ab2cbe
wesle <wesle@DESKTOP-V3NF8TU>
2017-03-11 21:58:46 -0300

    Add project files.

---
+++ AndreaVitorianoCoach/Controllers/VideosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AndreaVitorianoCoach.Controllers
+{
+    public class VideosController : Controller
+    {
+        // GET: Videos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AndreaVitorianoCoach/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AndreaVitorianoCoach.MvcApplication" Language="C#" %>
